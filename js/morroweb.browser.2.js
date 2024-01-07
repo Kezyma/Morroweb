cb1b51e06d37c72d32a5dbb10181b7656ac4d840
Alex Ashmore <kezyma@yahoo.co.uk>
2024-01-07 20:01:07 +0000

    starting to look better

--- js/morroweb.browser.2.js
+++ js/morroweb.browser.2.js
@@ -1,5 +1,35 @@
+
+//#region Constants
+const mw_specialisationMap = ["combat", "magic", "stealth"]
+const mw_attributeMap = ["strength", "intelligence", "willpower", "agility", "speed", "endurance", "personality", "luck"];
+//#endregion
+
+//#region Pre-Load
+function mw_preLoad() {
+    $.get({
+        url: "json/morroweb.gmst.json",
+        success: function (data) {
+            for (var s in data) {
+                mw_gmst_lookup[s.toLowerCase()] = data[s]
+            }
+        }
+    });
+}
+var mw_gmst_lookup = {}
+
+$(document).ready(function () {
+    mw_preLoad();
+    setInterval(mw_resize, 100);
+});
+
+function mw_resize() {
+    $(window).trigger("resize");
+}
+//#endregion
+
+//#region Table Functions
 function mw_newTable(id) {
-    return $(`<table id='mw_${id}_table' class='mwb-table table-sm table-borderless display nowrap table table-compact table-striped w-100 responsive'></table>`);
+    return $(`<table id='mw_${id}_table' class='mw-table table-sm table-borderless display table table-compact table-striped nowrap'></table>`);
 }
 
 function mw_newDataTable(container, id, headings, rows) {
@@ -13,7 +43,7 @@ function mw_newDataTable(container, id, headings, rows) {
         paging: false,
         scrollCollapse: true,
         scrollY: true,
-        scrollY: "calc(100vh - 320px)",
+        scrollY: "calc(100vh - 18rem)",
         scrollX: true,
         sScrollX: "100%"
     });
@@ -21,15 +51,53 @@ function mw_newDataTable(container, id, headings, rows) {
     // Initialise Bootstrap tooltips.
     var tooltipTriggerList = $(`#mw_${id}_table [data-bs-toggle="tooltip"]`);
     [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
 }
 
 function mw_showPanel(container) {
     $(".mwb-panel").hide();
     $(`#${container}`).show();
 }
+//#endregion
 
+//#region Icon Generators
+function mw_attribute_icon(attribute="") {
+    var a = attribute.toLowerCase().trim()
+    if (a != "" && a != "None") {
+        return `<img src='img/icons/attribute/attribute_${a}.png' data-bs-toggle="tooltip" data-bs-custom-class="mw-tooltip" data-bs-title='${mw_titleCase(attribute)}' />`
+    }
+    return "";
+}
+
+function mw_skill_icon(skill = "") {
+    var a = skill.toLowerCase().trim()
+    if (a != "" && a != "None") {
+        return `<img src='img/icons/skill/${a}.png' data-bs-toggle="tooltip" data-bs-custom-class="mw-tooltip" data-bs-title='${mw_titleCase(mw_gmst_lookup["sskill" + skill])}' />`
+    }
+    return "";
+}
+
+function mw_collapse_label(text = "") {
+    return `<div class='d-none d-xl-inline'>${text}</div>`;
+}
+
+function mw_id_format(id) {
+    return `<i>${id}</i>`
+}
+
+function mw_titleCase(str) {
+    str = str.toLowerCase().split(' ');
+    for (var i = 0; i < str.length; i++) {
+        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+    }
+    return str.join(' ');
+}
+//#endregion
+
+//#region Table Generators
 var mw_gmst_init = false;
 function mw_gmst_browser(container) {
+    mw_showPanel(container);
     if (!mw_gmst_init) {
         $.get({
             url: "json/morroweb.gmst.json",
@@ -47,11 +115,11 @@ function mw_gmst_browser(container) {
             }
         });
     }
-    mw_showPanel(container);
 }
 
 var mw_global_init = false;
 function mw_global_browser(container) {
+    mw_showPanel(container);
     if (!mw_global_init) {
         $.get({
             url: "json/morroweb.global.json",
@@ -69,5 +137,38 @@ function mw_global_browser(container) {
             }
         });
     }
-    mw_showPanel(container);
 }
+
+var mw_skill_init = false;
+function mw_skill_browser(container) {
+    mw_showPanel(container);
+    if (!mw_skill_init) {
+        $.get({
+            url: "json/morroweb.skill.json",
+            success: function (data) {
+                var columns = [
+                    { title: "Id", render: mw_id_format },
+                    {   title: "Name", 
+                        render: (i) => `${mw_skill_icon(i.toLowerCase())} ${mw_gmst_lookup["sskill" + i.toLowerCase()]}` 
+                    },
+                    {   title: "Attribute",
+                        render: (a) => `${mw_attribute_icon(mw_attributeMap[a])} ${mw_titleCase(mw_attributeMap[a])}`
+                    },
+                    { title: "Specialisation", render: (s) => mw_titleCase(mw_specialisationMap[s]) }
+                ];
+                var rows = [];
+                for (var key in data) {
+                    rows.push([
+                        key, 
+                        key,
+                        data[key]["Attribute"],
+                        data[key]["Specialisation"],
+                    ]);
+                }
+                mw_newDataTable(container, "skill", columns, rows);
+                mw_skill_init = true;
+            }
+        });
+    }
+}
+//#endregion
